379654894545a507ed9fd7a0f7afe12c120568e7
edobry <edobry@gmail.com>
2012-07-19 10:38:26 -0400

    Local inter-client play working

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var fs = require("fs");
-var 
 var stdin = process.stdin, stdout = process.stdout;
 
 var player1 = {};
@@ -9,6 +8,8 @@ var currPlayer = function (){ return (turn % 2 != 0) ? player1 : player2; };
 var grid = [" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "];
 
 var turn = 0;
+var lastMove = {};
+
 
 function start(){
 	stdout.write("Welcome to King Of Kmaps!\n\nPlease select a game mode:\n 1) H v H\n 2) H v M\n 3) M v M\n\n");
@@ -22,7 +23,7 @@ function start(){
 					input = input.toString().trim();
 					switch(input){
 						case "l":
-							setupGame("human", "human";
+							setupGame("human", "human");
 							break;
 						case "r":
 							p2 = "remote";
@@ -32,7 +33,7 @@ function start(){
 									case "h":
 										stdout.write("Waiting for connection...\n");
 										io = require('socket.io').listen(8080);
-										io.sockets.on('connection', function () {
+										io.sockets.on('connection', function (socket) {
 											stdout.write("Client connected!");
 											remote = socket;
 											remote.on('disconnect', function () {
@@ -69,7 +70,7 @@ function start(){
 }
 
 function setupGame(p1, p2){
-	function humanMove(callback){
+	function humanMove(){
 		stdout.write("\n");
 		prompt("x: ", function(input){
 			x = parseInt(input.toString().trim());
@@ -77,7 +78,6 @@ function setupGame(p1, p2){
 				y = parseInt(input.toString().trim());
 				
 				makeMove(x, y);
-				if(callback) callback(x, y);
 				nextTurn();
 			});
 		});
@@ -85,11 +85,12 @@ function setupGame(p1, p2){
 
 	function remoteMove(){
 		stdout.write("Waiting...");
-		remote.emit("request move");
 		remote.once("move made", function (move){
 			makeMove(move.x, move.y);
 			nextTurn();
 		});
+		remote.emit("move made", {"x": lastMove.x, "y": lastMove.y});
+		remote.emit("request move");
 	}
 	
 	function machineMove(){
@@ -132,8 +133,8 @@ function nextTurn(){
 		stdout.write("______________________________________________________\n\n");
 		stdout.write("Turn " + ++turn + ":\n"); 
 		
-		stdout.write("\nPlayer " + currPlayer().type + "'s turn:");
-		
+		stdout.write("\nPlayer " + currPlayer().type + "'s turn:\n");
+
 		currPlayer().makeMove();
 	}
 	else{
@@ -142,6 +143,7 @@ function nextTurn(){
 }
 
 function makeMove(x, y){
+	lastMove.x = x, lastMove.y = y;
 	grid[x+(8*y)] = currPlayer().symbol;
 	displayGrid();
 }
